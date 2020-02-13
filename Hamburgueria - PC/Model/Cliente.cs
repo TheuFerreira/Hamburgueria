c0585802c73ddbf795520f65a45618e1eb773a29
Ferreira <matheus.44300@gmail.com>
2020-02-12 21:45:25 -0300

    Update Inicial

---
+++ Hamburgueria - PC/Model/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SQLite;
+
+namespace Hamburgueria
+{
+    public class Cliente : Database
+    {
+        public static DataTable GetAll()
+        {
+            try
+            {
+                DataTable dt = new DataTable("Cliente");
+
+                connection.Open();
+
+                SQLiteCommand command = new SQLiteCommand(connection);
+                command.CommandText = "SELECT * FROM Cliente";
+
+                var r = new SQLiteDataAdapter(command);
+                r.Fill(dt);
+
+                connection.Close();
+
+                return dt;
+            }
+            catch (SQLiteException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Hamburgueria - PC/Model/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SQLite;
+
+namespace Hamburgueria
+{
+    public class Database
+    {
+        private static readonly string DATASOURCE = "DATASOURCE=hamburgueria.db";
+        protected static SQLiteConnection connection = new SQLiteConnection(DATASOURCE);
+    }
+}

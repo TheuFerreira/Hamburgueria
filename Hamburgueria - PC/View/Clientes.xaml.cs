fb126f3671496ce3fd285eeaad5054e58043460e
_theuferreira <matheus.44300@gmail.com>
2020-02-17 09:09:01 -0300

    Teste Clientes

--- Hamburgueria - PC/View/Clientes.xaml.cs
+++ Hamburgueria - PC/View/Clientes.xaml.cs
@@ -23,6 +23,32 @@ namespace Hamburgueria.View
             InitializeComponent();
 
             AddCliente.Click += delegate { new ClientesAdd().ShowDialog(); };
+
+            Item i = new Item("Matheus", "Avenida 1º de Junho", 162, "Centro", "Casa", "Igreja Matriz");
+            Item i2 = new Item("Paulo", "Jao Kisse", 666, "Inferno", "Casa", "Perto da Casa do Karalho");
+
+            List<Item> items = new List<Item>() { i, i2 };
+            GridClientes.ItemsSource = items;
+        }
+
+        public class Item
+        {
+            public string NOME { get; set; }
+            public string RUA { get; set; }
+            public int NUMERO { get; set; }
+            public string BAIRRO { get; set; }
+            public string COMPLEMENTO { get; set; }
+            public string REFERENCIA { get; set; }
+
+            public Item(string nome, string rua, int numero, string bairro, string complemento, string referencia)
+            {
+                this.NOME = nome;
+                this.RUA = rua;
+                this.REFERENCIA = referencia;
+                this.NUMERO = numero;
+                this.BAIRRO = bairro;
+                this.COMPLEMENTO = complemento;
+            }
         }
     }
 }

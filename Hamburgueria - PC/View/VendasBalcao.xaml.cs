c00bad980e54034c5cf386a017aacfaeba973954
jaokisse <61089612+jaokisse@users.noreply.github.com>
2020-03-26 20:50:40 -0300

    UPDATE AÍ

--- Hamburgueria - PC/View/VendasBalcao.xaml.cs
+++ Hamburgueria - PC/View/VendasBalcao.xaml.cs
@@ -23,5 +23,10 @@ namespace Hamburgueria.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

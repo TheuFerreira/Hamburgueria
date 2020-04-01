3543d526e83bbe19130c159f0fdbbf509d18d723
jaokisse <61089612+jaokisse@users.noreply.github.com>
2020-04-01 13:35:59 -0300

    update relatorios

--- Hamburgueria - PC/View/Relatorios.xaml.cs
+++ Hamburgueria - PC/View/Relatorios.xaml.cs
@@ -22,5 +22,10 @@ namespace Hamburgueria.View
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }

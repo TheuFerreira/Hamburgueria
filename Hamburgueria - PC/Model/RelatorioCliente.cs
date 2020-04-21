cf1833348d3f0bfaf3c94c8f8f3c278b96b3915f
Ferreira <matheus.44300@gmail.com>
2020-04-20 22:50:14 -0300

    Correção Relatórios

--- Hamburgueria - PC/Model/RelatorioCliente.cs
+++ Hamburgueria - PC/Model/RelatorioCliente.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections.Generic;
-using System;
 using System.Data.SQLite;
-using System.Windows;
+
 namespace Hamburgueria.Model
 {
     partial class Relatorio : Database
     {
         public class Cliente
         {
-            public DateTime Date { get; set; }
             public string Name { get; set; }
             public string Address { get; set; }
             public decimal TotalBruto { get; set; }
@@ -16,15 +14,13 @@ namespace Hamburgueria.Model
             public decimal Total { get; set; }
             public string Payment { get; set; }
 
-            public Cliente(DateTime date, string name, string address, decimal totalBruto, decimal desconto, decimal total, string payment)
+            public Cliente(string name, string address, decimal totalBruto, decimal desconto, decimal total)
             {
-                this.Date = date;
                 this.Name = name;
                 this.Address = address;
                 this.TotalBruto = totalBruto;
                 this.Desconto = desconto;
                 this.Total = total;
-                this.Payment = payment;
             }
         }
 
@@ -35,18 +31,31 @@ namespace Hamburgueria.Model
             connection.Open();
 
             SQLiteCommand command = new SQLiteCommand(connection);
-            command.CommandText = "" + 
-                "SELECT v.date, vd.name, vd.address, v.total_bruto, v.desconto, v.total, v.pagamento " +
+            command.CommandText = "" +
+                "SELECT vd.name, vd.address, v.total_bruto, v.desconto, v.total " +
                 "FROM venda_delivery vd " +
                 "INNER JOIN venda v ON v.id = vd.venda_id " +
                 "WHERE v.date like '" + date + "%';";
 
             var r = command.ExecuteReader();
             while (r.Read())
-                p.Add(new Relatorio.Cliente(r.GetDateTime(0), r.GetString(1), r.GetString(2), r.GetDecimal(3), r.GetDecimal(4), r.GetDecimal(5), r.GetString(6)));
+                p.Add(new Relatorio.Cliente(r.GetString(1), r.GetString(2), r.GetDecimal(3), r.GetDecimal(4), r.GetDecimal(5)));
             r.Close();
 
             connection.Close();
+
+            for (int i = 0; i < p.Count - 1; i++)
+            {
+                if (p[i].Name == p[i + 1].Name && p[i].Address == p[i + 1].Address)
+                {
+                    p[i].TotalBruto += p[i + 1].TotalBruto;
+                    p[i].Desconto += p[i + 1].Desconto;
+                    p[i].Total += p[i + 1].Total;
+
+                    p.RemoveAt(i + 1);
+                    i -= 1;
+                }
+            }
 
             return p;
         }
@@ -66,10 +75,23 @@ namespace Hamburgueria.Model
 
             var r = command.ExecuteReader();
             while (r.Read())
-                p.Add(new Relatorio.Cliente(r.GetDateTime(0), r.GetString(1), r.GetString(2), r.GetDecimal(3), r.GetDecimal(4), r.GetDecimal(5), r.GetString(6)));
+                p.Add(new Relatorio.Cliente(r.GetString(1), r.GetString(2), r.GetDecimal(3), r.GetDecimal(4), r.GetDecimal(5)));
             r.Close();
 
             connection.Close();
+
+            for (int i = 0; i < p.Count - 1; i++)
+            {
+                if (p[i].Name == p[i + 1].Name && p[i].Address == p[i + 1].Address)
+                {
+                    p[i].TotalBruto += p[i + 1].TotalBruto;
+                    p[i].Desconto += p[i + 1].Desconto;
+                    p[i].Total += p[i + 1].Total;
+
+                    p.RemoveAt(i + 1);
+                    i -= 1;
+                }
+            }
 
             return p;
         }
